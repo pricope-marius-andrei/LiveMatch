2dfd40b8b887c8fa6497be2ccf30edbd39d323cf
MariusPricope <mariuspricope66@gmai.com>
2022-09-07 09:30:57 +0300

    redesign

--- liveMatches.js
+++ liveMatches.js
@@ -4,6 +4,7 @@ import {firstTeamName, score, secondTeamName, homeTeamLogo, awayTeamLogo, nextMa
 var id = 0
 
 const data = await getData()
+var statusMatch = data.response[id].fixture.status.short
 
 console.log(data)
 for(let i = 0 ; i < data.results; i++) {
@@ -12,7 +13,9 @@ for(let i = 0 ; i < data.results; i++) {
         secondTeamName.textContent = data.response[i].teams.away.name
         homeTeamLogo.src = data.response[i].teams.home.logo
         awayTeamLogo.src = data.response[i].teams.away.logo
-        score.textContent = data.response[i].goals.home + "-" + data.response[i].goals.away
+        statusMatch = data.response[i].fixture.status.short
+        if(statusMatch !== "NS")
+            score.textContent = data.response[i].goals.home + "-" + data.response[i].goals.away
         id = i;
         break;
        
@@ -26,8 +29,11 @@ nextMatch.addEventListener("click", function() {
             secondTeamName.textContent = data.response[i].teams.away.name
             homeTeamLogo.src = data.response[i].teams.home.logo
             awayTeamLogo.src = data.response[i].teams.away.logo
-            score.textContent = data.response[i].goals.home + "-" + data.response[i].goals.away
+            statusMatch = data.response[i].fixture.status.short
+            if(statusMatch !== "NS")
+                score.textContent = data.response[i].goals.home + "-" + data.response[i].goals.away
             id = i;
+            statusMatch = data.response[id].fixture.status.short
             break;
         }
     }
@@ -40,21 +46,27 @@ previousMatch.addEventListener("click", function() {
             secondTeamName.textContent = data.response[i].teams.away.name
             homeTeamLogo.src = data.response[i].teams.home.logo
             awayTeamLogo.src = data.response[i].teams.away.logo
-            score.textContent = data.response[i].goals.home + "-" + data.response[i].goals.away
+            statusMatch = data.response[i].fixture.status.short
+            if(statusMatch !== "NS")
+                score.textContent = data.response[i].goals.home + "-" + data.response[i].goals.away
             id = i;
+            
             break;
         }
     }
 })
 
-var timer = 100;
-
 function UpdateScore()
 {
     window.requestAnimationFrame(UpdateScore);
-    score.textContent = data.response[id].goals.home + "-" + data.response[id].goals.away
-    time.textContent = data.response[id].fixture.status.elapsed + 2 + ":" + "00"
-
+    statusMatch = data.response[id].fixture.status.short
+    if(statusMatch === "NS") {
+        time.textContent = "Not started"
+    }
+    else {
+        score.textContent = data.response[id].goals.home + "-" + data.response[id].goals.away
+        time.textContent = data.response[id].fixture.status.elapsed + ":" + "00"
+    }
 }
 
 window.requestAnimationFrame(UpdateScore)
